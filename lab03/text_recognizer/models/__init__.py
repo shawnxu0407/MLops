51596ad569b26424beebafed3e0e7e4b6b5e1bd3
Shawn Xu <148259134+wudidazuishou@users.noreply.github.com>
2025-02-17 18:32:47 -0500

    Delete lab03 directory

--- lab03/text_recognizer/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Models for character and text recognition in images."""
-from .mlp import MLP
-
-from .cnn import CNN
-from .line_cnn_simple import LineCNNSimple
-
-
-from .resnet_transformer import ResnetTransformer
